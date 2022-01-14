2b3bde6e93cfcc20198a9d2d3de123047970b19d
Aykut Fatin <aykut.fatin@abv.bg>
2022-01-14 12:28:37 +0200

    Installed react router dom

--- MERN/client/src/App.js
+++ MERN/client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-    return <h1>Let's start!</h1>;
+const App = () => {
+    return (
+        <Router>
+            <h1>Let's start!</h1>
+        </Router>
+    );
 }
 
 export default App;
