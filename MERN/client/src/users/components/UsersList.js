a27dd6a112b4e196fb50362e0f82f53dd611ccb0
Aykut Fatin <aykut.fatin@abv.bg>
2022-02-01 10:34:17 +0200

    Adjusted some styles in Card and Avatar components

--- MERN/client/src/users/components/UsersList.js
+++ MERN/client/src/users/components/UsersList.js
@@ -3,21 +3,23 @@ import React from 'react';
 import './UsersList.css';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
 
 function UsersList(props) {
     if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No users found.</h2>
+                <Card>
+                    <h2>No users found.</h2>
+                </Card>
             </div>
         );
     }
 
-
     return (
         <ul className="users-list">
             {props.items.map(user => (
-                <UserItem 
+                <UserItem
                     key={user.id}
                     id={user.id}
                     image={user.image}
