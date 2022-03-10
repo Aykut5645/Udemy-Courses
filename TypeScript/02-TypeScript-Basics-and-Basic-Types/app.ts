79fe2ba19cdc5504d527c00ce10872b84689fcc5
Aykut Fatin <aykut.fatin@abv.bg>
2022-03-10 18:40:55 +0200

    Added new code related to learning typescript types

--- TypeScript/02-TypeScript-Basics-and-Basic-Types/app.ts
+++ TypeScript/02-TypeScript-Basics-and-Basic-Types/app.ts
@@ -1,29 +1,20 @@
-type Combinable = number | string; // custom type (type aliases)
-type ConversionDescriptor = 'as-number' | 'as-text';
+// => unknown type; comparison between 'unknown' and 'any'
+let userInput: unknown;
+let userName: string;
 
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversionDescriptor
-) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    // if (resultConversion === 'as-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+// if (typeof userInput === 'string') {
+//     userName = userInput;
+// }
+
+//userName = userInput; // here we have a problem; with 'any' type we would not have this problem';
+// 'unknown' type is more flexible than 'any' type;
+
+// => never type; comparison between 'never' and 'void'
+function generateError(msg: string, code: Number): never {
+    throw { message: msg, errorCode: code };
+    while (true) { } // infinite loop => another function that returns nothing
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+let result = generateError('An error ocurred', 500) // this function returns 'never' in spite of when we hover over it we see 'void' type
+console.log(result);
+// generateError function never produces a value, because by throwing error we cancel its execution
