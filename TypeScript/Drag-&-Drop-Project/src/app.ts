d46029596e3d7f0e522b6f0bfc53c465d860a6ea
Aykut Fatin <aykut.fatin@abv.bg>
2022-05-29 22:49:41 +0300

    Added logic related to showing form in Drag-&-Drop-Project

--- TypeScript/Drag-&-Drop-Project/src/app.ts
+++ TypeScript/Drag-&-Drop-Project/src/app.ts
@@ -1,9 +1,25 @@
-// class ProjectInput {
-//     templateElement: HTMLTemplateElement;
-//     rootElement: HTMLDivElement;
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    rootElement: HTMLDivElement;
+    element: HTMLFormElement;
 
-//     constructor() {
-//         this.templateElement = document.getElementById('');
-//         this.rootElement = document.getElementById('');
-//     }
-// }
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.rootElement = document.getElementById('app')! as HTMLDivElement;
+
+        // const cloneNode = this.templateElement.content.cloneNode(true);
+        const importedNote = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNote.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.rootElement.appendChild(this.element);
+        // this.rootElement.insertAdjacentElement('afterbegin', node.firstElementChild!);
+    }
+}
+
+new ProjectInput();
