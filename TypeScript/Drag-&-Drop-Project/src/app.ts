383a7c35c97f38aa7747dcd58323b76bea2c6d28
Aykut Fatin <aykut.fatin@abv.bg>
2022-05-29 20:55:40 +0300

    Added structure of files (html, css and etc) related to basic Drag-&-Drop-Project

---
+++ TypeScript/Drag-&-Drop-Project/src/app.ts
@@ -0,0 +1,9 @@
+// class ProjectInput {
+//     templateElement: HTMLTemplateElement;
+//     rootElement: HTMLDivElement;
+
+//     constructor() {
+//         this.templateElement = document.getElementById('');
+//         this.rootElement = document.getElementById('');
+//     }
+// }
