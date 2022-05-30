af487d776ed4e218de2579d43f9ffa06f7af0a13
Aykut Fatin <aykut.fatin@abv.bg>
2022-05-30 20:40:13 +0300

    Added logic for retrieving data from form in Drag-&-Drop-Project

--- TypeScript/Drag-&-Drop-Project/src/app.ts
+++ TypeScript/Drag-&-Drop-Project/src/app.ts
@@ -1,24 +1,69 @@
+// autobind decorator
+//...
+
+// Project input class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     rootElement: HTMLDivElement;
     element: HTMLFormElement;
 
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLTextAreaElement;
+    peopleInputElement: HTMLInputElement;
+
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.rootElement = document.getElementById('app')! as HTMLDivElement;
 
-        // const cloneNode = this.templateElement.content.cloneNode(true);
-        const importedNote = document.importNode(
+        const importedNode = document.importNode(
             this.templateElement.content,
             true
         );
-        this.element = importedNote.firstElementChild as HTMLFormElement;
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLTextAreaElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value.trim();
+        const enteredDescription = this.descriptionInputElement.value.trim();
+        const enteredPeople = Number(this.peopleInputElement.value.trim());
+
+        if (!enteredTitle || !enteredDescription || !enteredPeople) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInputs = this.gatherUserInput();
+        if (Array.isArray(userInputs)) {
+            // const [title, description, people] = userInputs;
+            this.clearInputs();
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
         this.rootElement.appendChild(this.element);
-        // this.rootElement.insertAdjacentElement('afterbegin', node.firstElementChild!);
     }
 }
 
