02fe4e7790f5ca21643cb20c8355df2add9a42b9
Aykut Fatin <aykut.fatin@abv.bg>
2022-06-02 20:49:28 +0300

    Added listeners related to dragstart and dragend event in ProjectItem class

--- TypeScript/Drag-&-Drop-Project/src/app.ts
+++ TypeScript/Drag-&-Drop-Project/src/app.ts
@@ -1,3 +1,14 @@
+// Drag & Drop Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+interface Dragtarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {
     Active,
@@ -127,7 +138,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // Project Item Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     private project: Project;
 
     get persons() {
@@ -145,7 +156,18 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure() { }
+    dragStartHandler(event: DragEvent) {
+        console.log(event);
+    }
+
+    dragEndHandler(_: DragEvent) {
+        console.log('dragend');
+    }
+
+    configure() {
+        this.element.addEventListener("dragstart", this.dragStartHandler.bind(this));
+        this.element.addEventListener("dragend", this.dragEndHandler.bind(this));
+    }
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
@@ -225,20 +247,17 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
             value: enteredTitle,
             required: true
         };
-        console.log(titleValidatable);
         const descriptionValidatable: Validatable = {
             value: enteredDescription,
             required: true,
             minLength: 5
         };
-        console.log(descriptionValidatable);
         const peopleValidatable: Validatable = {
             value: enteredPeople,
             required: true,
             min: 1,
             max: 5
         };
-        console.log(peopleValidatable);
 
         if (
             !validate(titleValidatable) ||
