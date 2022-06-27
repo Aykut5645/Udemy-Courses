f7ba1ad5d4404fc4bc0e602008a5c11b4699d0da
Aykut5645 <aykut.fatin@abv.bg>
2022-06-27 22:23:57 +0300

    Added code related to directivies-deep-dive lesson (angular-course)

---
+++ Angular/03-Directivies-Deep-Dive/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+    selector: '[appBasicHighlight]',
+})
+export class BasicHighlightDirective implements OnInit {
+    constructor(private elRef: ElementRef) {}
+
+    ngOnInit() {
+        this.elRef.nativeElement.style.backgroundColor = 'green';
+    }
+}
