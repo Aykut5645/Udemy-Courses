13db213d68d73236aeca10c5ed1d0d5422355c03
Aykut5645 <aykut.fatin@abv.bg>
2022-07-04 14:19:27 +0300

    Added code related to angular directivies in angular course

--- Angular/03-Directivies-Deep-Dive/src/app/better-highlight/better-highlight.directive.ts
+++ Angular/03-Directivies-Deep-Dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,16 +1,42 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import {
+    Directive,
+    ElementRef,
+    Renderer2,
+    OnInit,
+    HostListener,
+    HostBinding,
+    Input,
+} from '@angular/core';
 
 @Directive({
     selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+    @Input() defaultColor = 'transparent';
+    @Input() highlightColor = 'blue';
+    @HostBinding('style.backgroundColor') backgroundColor = this.defaultColor;
+
     constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit() {
-        this.renderer.setStyle(
-            this.elRef.nativeElement,
-            'background-color',
-            'blue'
-        );
+        this.backgroundColor = this.defaultColor;
+    }
+
+    @HostListener('mouseenter') onMouseEnter(event: Event) {
+        // this.renderer.setStyle(
+        //     this.elRef.nativeElement,
+        //     'background-color',
+        //     'blue'
+        // );
+        this.backgroundColor = this.highlightColor;
+    }
+
+    @HostListener('mouseleave') onMouseLeave(event: Event) {
+        // this.renderer.setStyle(
+        //     this.elRef.nativeElement,
+        //     'background-color',
+        //     'transparent'
+        // );
+        this.backgroundColor = this.defaultColor;
     }
 }
