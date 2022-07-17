48287846773e78bcc78c23400cfa08ce7a61b223
Aykut5645 <aykut.fatin@abv.bg>
2022-07-18 00:51:11 +0300

    Feed with some life recipe-detail-component and render the content dynamically

--- Angular/COURSE-PROJECT/src/app/header/header.component.ts
+++ Angular/COURSE-PROJECT/src/app/header/header.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    @Output() selectedFeature = new EventEmitter<string>();
+
+    constructor() {}
+
+    onNavClick(feature: string) {
+        this.selectedFeature.emit(feature);
+    }
+}
